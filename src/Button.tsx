4fa1c52f0710eee1f7c788f923e5fe57f1d0227b
Humberto <humbertogach13@gmail.com>
2024-09-07 14:54:38 -0600

    Tercera clase

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,11 @@
-function Button({ value, onClick }: { value: string; onClick: () => void}) {
-    return <button onClick={onClick}>{value}</button>;
-}
+import React from 'react';
+import { ButtonProps } from './ButtonProps'; // Importamos la interfaz desde su propio archivo
 
+const Button: React.FC<ButtonProps> = ({ value, onClick}) => {
+    return (
+        <button className="operator" onClick={onClick}>
+            {value}
+        </button>
+    );
+};
 export default Button;
